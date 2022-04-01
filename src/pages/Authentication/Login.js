ea35e4de8281881c47f0e535e50e767a295d1ad1
roaaAli97 <roaaa5824@gmail.com>
2022-04-01 14:42:33 +0200

    Update Login.js
    Updating the UI so that it contains email and password only

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -12,19 +12,15 @@ import { withRouter, Link } from "react-router-dom"
 // availity-reactstrap-validation
 import { AvForm, AvField } from "availity-reactstrap-validation"
 
-//Social Media Imports
-import { GoogleLogin } from "react-google-login"
-// import TwitterLogin from "react-twitter-auth"
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props"
 
 // actions
-import { loginUser, socialLogin } from "../../store/actions"
+import { loginUser } from "../../store/actions"
 
 // import images
 import logo from "../../assets/images/logo-sm.svg"
 
 //Import config
-import { facebook, google } from "../../config"
+
 import CarouselPage from "./CarouselPage"
 
 const Login = props => {
@@ -33,49 +29,19 @@ const Login = props => {
   const { error } = useSelector(state => ({
     error: state.Login.error,
   }))
-
+ console.log(error)
   // handleValidSubmit
   const handleValidSubmit = (event, values) => {
+    
+    event.preventDefault()
     dispatch(loginUser(values, props.history))
   }
 
-  const signIn = (res, type) => {
-    if (type === "google" && res) {
-      const postData = {
-        name: res.profileObj.name,
-        email: res.profileObj.email,
-        token: res.tokenObj.access_token,
-        idToken: res.tokenId,
-      }
-      dispatch(socialLogin(postData, props.history, type))
-    } else if (type === "facebook" && res) {
-      const postData = {
-        name: res.name,
-        email: res.email,
-        token: res.accessToken,
-        idToken: res.tokenId,
-      }
-      dispatch(socialLogin(postData, props.history, type))
-    }
-  }
-
-  //handleGoogleLoginResponse
-  const googleResponse = response => {
-    signIn(response, "google")
-  }
-
-  //handleTwitterLoginResponse
-  // const twitterResponse = e => {}
-
-  //handleFacebookLoginResponse
-  const facebookResponse = response => {
-    signIn(response, "facebook")
-  }
-
+  
   return (
     <React.Fragment>
       <MetaTags>
-        <title>Login | Minia - React Admin & Dashboard Template</title>
+        <title>Login</title>
       </MetaTags>
       <div className="auth-page">
         <Container fluid className="p-0">
@@ -86,14 +52,10 @@ const Login = props => {
                   <div className="d-flex flex-column h-100">
                     <div className="mb-4 mb-md-5 text-center">
                       <Link to="/dashboard" className="d-block auth-logo">
-                        <img src={logo} alt="" height="28" /> <span className="logo-txt">Minia</span>
+                        <img src={logo} alt="" height="28" /> <span className="logo-txt">Exiniti</span>
                       </Link>
                     </div>
-                    <div className="auth-content my-auto">
-                      <div className="text-center">
-                        <h5 className="mb-0">Welcome Back !</h5>
-                        <p className="text-muted mt-2">Sign in to continue to Minia.</p>
-                      </div>
+                    
                       <AvForm
                         className="custom-form mt-4 pt-2"
                         onValidSubmit={(e, v) => {
@@ -117,11 +79,7 @@ const Login = props => {
                             <div className="flex-grow-1">
                               <label className="form-label">Password</label>
                             </div>
-                            <div className="flex-shrink-0">
-                              <div className="">
-                                <Link to="/auth-recoverpw" className="text-muted">Forgot password?</Link>
-                              </div>
-                            </div>
+                            
                           </div>
 
                           <div className="mb-3">
@@ -135,61 +93,13 @@ const Login = props => {
                             />
                           </div>
                         </div>
-                        <div className="row mb-4">
-                          <div className="col">
-                            <div className="form-check">
-                              <input className="form-check-input" type="checkbox" id="remember-check" />
-                              <label className="form-check-label" htmlFor="remember-check">
-                                Remember me
-                              </label>
-                            </div>
-                          </div>
-
-                        </div>
+                        
                         <div className="mb-3">
                           <button className="btn btn-primary w-100 waves-effect waves-light" type="submit">Log In</button>
                         </div>
                       </AvForm>
 
-                      <div className="mt-4 text-center">
-                        <h5 className="font-size-14 mb-3">Sign in with</h5>
-
-                        <ul className="list-inline">
-                          <li className="list-inline-item">
-                            <FacebookLogin
-                              appId={facebook.APP_ID}
-                              autoLoad={false}
-                              callback={facebookResponse}
-                              render={renderProps => (
-                                <Link
-                                  to="#"
-                                  className="social-list-item bg-primary text-white border-primary"
-                                  onClick={renderProps.onClick}
-                                >
-                                  <i className="mdi mdi-facebook" />
-                                </Link>
-                              )}
-                            />
-                          </li>
-
-                          <li className="list-inline-item">
-                            <GoogleLogin
-                              clientId={google.CLIENT_ID}
-                              render={renderProps => (
-                                <Link
-                                  to="#"
-                                  className="social-list-item bg-danger text-white border-danger"
-                                  onClick={renderProps.onClick}
-                                >
-                                  <i className="mdi mdi-google" />
-                                </Link>
-                              )}
-                              onSuccess={googleResponse}
-                              onFailure={() => { }}
-                            />
-                          </li>
-                        </ul>
-                      </div>
+                     
 
                       <div className="mt-5 text-center">
                         <p className="text-muted mb-0">Don't have an account ? <Link to="/register"
@@ -200,7 +110,7 @@ const Login = props => {
                       <p className="mb-0">© {new Date().getFullYear()} Minia . Crafted with <i className="mdi mdi-heart text-danger"></i> by Themesbrand</p>
                     </div>
                   </div>
-                </div>
+                
               </div>
             </Col>
             <CarouselPage />
