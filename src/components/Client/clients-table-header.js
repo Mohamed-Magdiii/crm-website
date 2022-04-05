c1220c31e7ce6b02d868195fad03a3e6591430ba
roaaAli97 <roaaa5824@gmail.com>
2022-04-05 11:53:19 +0200

    Client  Table main Header

---
+++ src/components/Client/clients-table-header.js
@@ -0,0 +1,19 @@
+
+function ClientTableHeader(){
+  return(
+      <thead>
+       <tr>
+       <th><input type="checkbox"/></th>
+       <th>Register Date</th>
+       <th>Name</th>
+       <th>type</th>
+       <th>email</th>
+       <th>phone number</th>
+       <th>Sales</th>
+       <th>Source</th>
+       <th>KYC</th>
+       </tr>
+    </thead>
+    )
+  }
+export default ClientTableHeader
