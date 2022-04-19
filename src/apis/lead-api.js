cca8588e88902edd548168713738330ec4024209
roaaAli97 <roaaa5824@gmail.com>
2022-04-19 12:19:21 +0200

    Modifying lead-api-POR-14
    I used axios instead of fetch

--- src/apis/lead-api.js
+++ src/apis/lead-api.js
@@ -1,28 +1,16 @@
 import * as axiosHelper from "./api_helper";
 export const addNewLead = async(values) => {
-  const result = await fetch("http://localhost:3001/api/v1/crm/leads", {
-    method:"POST",
-    mode:"cors",
-         
-    headers:{  
-      "Content-Type":"application/json",
-      "Accept":"application/json"
-    },
-    body:JSON.stringify({
-      firstName:values.firstName,
-      lastName:values.lastName,
-      email:values.email,
-      phone:values.phone,
-      country:values.country,
-      password:values.password
-    })
-  });
-  const data = await result.json();
+  console.log(values);
+  const data = await axiosHelper.post("/leads", {...values});
+  if (data.isError){
+    throw new Error(data.message);
+  }
+  
   return data;
 };
 export const fetchLeadsFromAPI = async ( { sizePerPage, currentPage })=>{
   
-  const result = await  axiosHelper.get(`/api/v1/crm/leads?limit=${sizePerPage}&page=${currentPage}`);
+  const result = await  axiosHelper.get(`/leads?limit=${sizePerPage}&page=${currentPage}`);
   
   return result;
 };
