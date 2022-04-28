827e031a049fb1bc40f98e53384e11b89006505d
mostafa-sultan <mostafasoltan82@gmail.com>
2022-04-28 08:44:31 +0200

    update team title and manager modal POR-77

---
+++ src/pages/Teams/loadOptions.js
@@ -0,0 +1,69 @@
+import * as teamsApi from "../../apis/teams";
+
+
+const optionsPerPage = 3;
+
+const loadOptions = async (search, page) => {
+  //   await sleep(1000);
+
+  let output = [];
+
+  let filteredOptions;
+  let data;
+  if (!search) {
+    data = await teamsApi
+      .getManagers({
+        payload: {
+          page: page,
+          limit: 5,
+        },
+      })
+      .then((results) => {
+        //do any results transformations
+        return results;
+      });
+    data.docs?.map(function (item) { 
+      output.push({
+        value: item._id,
+        label: item.firstName,
+      });
+    });
+    filteredOptions = output;
+  } else { 
+    output = [];
+    data = {};
+    data = await teamsApi
+      .getManagers({
+        payload: {
+          page: page,
+          limit: 3,
+          searchText: search,
+        },
+      })
+      .then((results) => {
+        //do any results transformations
+        return results;
+      });
+    data.docs?.map(function (item) {
+      // output[item] = obj[item]['value']
+      output.push({
+        value: item._id,
+        label: item.firstName,
+      });
+    });
+
+    filteredOptions = output; 
+  }
+
+  const hasMore = Math.ceil(filteredOptions.length / optionsPerPage) > page;
+  const slicedOptions = filteredOptions.slice(
+    (page - 1) * optionsPerPage,
+    page * optionsPerPage
+  ); 
+  return {
+    options: filteredOptions,
+    hasMore: data.hasNextPage,
+  };
+};
+
+export default loadOptions;
