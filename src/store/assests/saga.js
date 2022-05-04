0527332ca39d69122471abec73978b5ad16c0e75
roaaAli97 <roaaa5824@gmail.com>
2022-05-04 11:55:48 +0200

    adding deleteAsset function to the saga-POR-78

--- src/store/assests/saga.js
+++ src/store/assests/saga.js
@@ -2,13 +2,13 @@ import {
   call, put, takeEvery 
 } from "redux-saga/effects";
 import { 
-  fetchAssestsSuccess, apiError, addNewSymbolSuccess
+  fetchAssestsSuccess, apiError, addNewSymbolSuccess, deleteSymbolDone
 } from "./actions";
 import { 
-  getAssests, addNewSymbol, updateSymbol
+  getAssests, addNewSymbol, updateSymbol, deleteSymbol
 } from "apis/assets";
 import {
-  FETCH_ASSESTS_START, ADD_NEW_SYMBOL, EDIT_SYMBOL_START
+  FETCH_ASSESTS_START, ADD_NEW_SYMBOL, EDIT_SYMBOL_START, DELETE_SYMBOL_START
 } from "./actionsType";
 function * fetchAssest(params){
   try {
@@ -44,9 +44,27 @@ function * editAsset(params){
   }
   
 }
+function * deleteAsset(params){
+  try {
+    const data = yield call(deleteSymbol, params);
+
+    const { result } = data;
+    yield put(deleteSymbolDone({
+      result,
+      id:params.payload,
+      
+    }));
+    
+  } catch (error){
+    yield put(apiError("An error happned during deleting this record"));
+  }
+  
+
+}
 function * assestSaga(){
   yield  takeEvery(FETCH_ASSESTS_START, fetchAssest);
   yield  takeEvery(ADD_NEW_SYMBOL, addNewAssest);
   yield  takeEvery(EDIT_SYMBOL_START, editAsset);
+  yield takeEvery(DELETE_SYMBOL_START, deleteAsset);
 }
 export default assestSaga;
