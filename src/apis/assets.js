f3b9a94ecbb969b0a8dfc29a12d159a3971d96b8
roaaAli97 <roaaa5824@gmail.com>
2022-05-09 16:41:14 +0200

    using axiosHelper instead of fetch-POR-79
    I changed the code and used the api_helper instead of the fetch

--- src/apis/assets.js
+++ src/apis/assets.js
@@ -2,35 +2,30 @@ import qs from "qs";
 import * as axiosHelper from "./api_helper";
 export const getAssets = async ({ payload })=>{
   const data = await axiosHelper.get(`/assets/?${qs.stringify(payload)}`, { crypto: true });
-  console.log(data);
   return data;
 };
 export const addNewSymbol = async (values)=>{
   
-  const result = await fetch("http://localhost:3001/api/v1/crypto/assets", {
-    method:"POST",
-    headers:{
-      "Content-type" :"application/json"
+  const data = await axiosHelper.post("/assets", {
+    name:values.name,
+    symbol: values.symbol,
+    description: values.description,
+    markup: values.markup,
+    explorerLink:values.explorerLink,
+    fee: {
+      deposit:values.depositFee,
+      withdrawal:values.withdrawFee
     },
-    body: JSON.stringify({
-      name:values.name,
-      symbol: values.symbol,
-      description: values.description,
-      markup: values.markup,
-      fee: {
-        deposit:values.depositFee,
-        withdrawal:values.withdrawFee
-      },
-      explorerLink: values.explorerLink,
-      minAmount: {
-        deposit :values.minDepositAmount,
-        withdrawal:values.minWithdrawAmount
-      },
-      isCrypto:true,
+
+    minAmount: {
+      deposit :values.minDepositAmount,
+      withdrawal:values.minWithdrawAmount
+    },
+    isCrypto:true,
       
-    })
-  });
-  const data = await result.json();
+  }, { crypto:true }
+  );
+  
   if (data.code === 500){
     throw new Error("Please Enter Valid data");
   }
@@ -42,30 +37,24 @@ export const updateSymbol = async ({ payload })=>{
   const { id, values } = payload;
   
   const { name, markup, explorerLink, description } = values;
-  const result = await fetch(`http://localhost:3001/api/v1/crypto/assets/${id}`, {
-    method:"PATCH",
-    headers:{
-      "Content-type" :"application/json"
+  const data = await axiosHelper.patch(`/assets/${id}`, {
+  
+    name,
+    description,
+    markup,
+    explorerLink,
+    fee: {
+      deposit:values.depositFee,
+      withdrawal:values.withdrawFee
     },
-    body: JSON.stringify({
-      name,
-      description,
-      markup,
-      explorerLink,
-      fee: {
-        deposit:values.depositFee,
-        withdrawal:values.withdrawFee
-      },
-      minAmount: {
-        deposit :values.minDepositAmount,
-        withdrawal:values.minWithdrawAmount
-      },
+    minAmount: {
+      deposit :values.minDepositAmount,
+      withdrawal:values.minWithdrawAmount
+    },
     
       
-    })
-  });
-  
-  const data = await result.json();
+  }, { crypto:true });
+
   if (data.code === 500){
     throw new Error("Please Enter Valid data");
   }
@@ -73,11 +62,11 @@ export const updateSymbol = async ({ payload })=>{
 }; 
 export const deleteSymbol = async ({ payload })=>{
  
-  const result = await fetch(`http://localhost:3001/api/v1/crypto/assets/${payload}`, {
-    method:"DELETE"
+  const result = await axiosHelper.del(`/assets/${payload}`, {
+    crypto:true
   });
-  const data = await result.json();
+
   
-  return data;
+  return result;
   
 };
