7af334d3f4d2aafce54f945a8fae600668bd5a85
roaaAli97 <roaaa5824@gmail.com>
2022-05-12 11:06:24 +0200

    Deposit page -POR-95

---
+++ src/pages/Transactions/AddDepositForm.js
@@ -0,0 +1,113 @@
+import { useDispatch, connect } from "react-redux";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  UncontrolledAlert,
+} from "reactstrap";
+
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import { fetchGatewaysStart } from "store/gateway/action";
+
+function DepositForm(props){
+
+  const [addModal, setDepositModal] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleAddDeposit = (event, values) => {
+    event.preventDefault();
+    
+    
+  }; 
+
+  const toggleAddModal = () => {
+    setDepositModal(!addModal);
+  };
+  useEffect(()=>{
+    dispatch(fetchGatewaysStart());
+  }, []);
+  useEffect(() => {
+    
+  }, [props.addSymbolSuccessMessage]);
+  
+  return (
+    <React.Fragment >
+      <Link to="#" className="btn btn-light" onClick={toggleAddModal}><i className="bx bx-plus me-1"></i> Add Deposit</Link>
+      <Modal isOpen={addModal} toggle={toggleAddModal} centered={true}>
+        <ModalHeader toggle={toggleAddModal} tag="h4">
+          Add Deposit
+        </ModalHeader>
+        <ModalBody >
+          <AvForm
+            className='p-4'
+            onValidSubmit={(e, v) => {
+              handleAddDeposit(e, v);
+            }}
+          >
+            
+              
+            <div className="mb-3">
+              <AvField
+                name="gateway"
+                label="Gateway"
+                placeholder="gateway"
+                type="select"
+                errorMessage="Enter valid gateway"
+                validate={{ required: { value: true } }}
+              >
+                {Object.keys(props.gateways).map((key)=>{
+                  return <option key={key}>{props.gateways[key]}</option>;
+                })}
+              </AvField>
+            </div>
+              
+               
+            <div className="mb-3">
+              <AvField
+                name="amount"
+                label="Amount"
+                placeholder="Amount"
+                type="text"
+                errorMessage="Enter Valid Amount"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            <div className="mb-3">
+              <AvField
+                name="note"
+                label="Note"
+                placeholder="Note"
+                type="text"
+                errorMessage="Enter Valid Note"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+    
+            <div className='text-center pt-3 p-2'>
+              <Button  type="submit" color="primary" className="">
+                    Add Deposit
+              </Button>
+            </div>
+          </AvForm>
+          {props.error && <UncontrolledAlert color="danger">
+            <i className="mdi mdi-block-helper me-2"></i>
+            {props.error}
+          </UncontrolledAlert>}
+          {props.addSymbolSuccessMessage && <UncontrolledAlert color="success">
+            <i className="mdi mdi-check-all me-2"></i>
+              Deposit Added successfully !!!
+          </UncontrolledAlert>}
+        </ModalBody>
+      </Modal>
+    </React.Fragment>
+  );
+}
+const mapStateToProps = (state) => ({
+  gateways:state.gatewayReducer.gateways || [],
+
+});
+export default connect(mapStateToProps, null)(DepositForm);
---
+++ src/pages/Transactions/Deposit.js
@@ -0,0 +1,115 @@
+import React, {
+  useState, useEffect, useCallback
+} from "react";
+import { useDispatch, connect } from "react-redux";
+import { fetchClientsStart } from "store/client/actions";
+import { fetchWalletStart } from "store/wallet/action";
+import {
+  Row, Col, Card, CardBody, CardHeader, Dropdown, DropdownMenu, DropdownToggle, DropdownItem
+} from "reactstrap";
+import AddDepositForm from "./AddDepositForm";
+import { debounce } from "lodash";
+function Deposit(props){
+  const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState("");
+  const [menu, setMenu] = useState(false);
+  const [searchDropDown, setSearchDropDown] = useState(false);
+  const toggleSearchDropDown = ()=>setSearchDropDown(preValue=>!preValue);
+  const toggle = ()=>setMenu(preValue=>!preValue);
+  useEffect(()=>{
+    if (searchInput !== ""){
+      dispatch(fetchClientsStart({
+        searchText:searchInput
+      }));
+    }
+   
+  }, [searchInput]);
+  const handleSearchInput = (e)=>{
+  
+    setSearchInput(e.target.value);
+    
+  };
+  const debouncedChangeHandler = useCallback(
+    debounce(handleSearchInput, 1000), []
+  );
+  
+  const selectClient = (id)=>{
+    
+    dispatch(fetchWalletStart({
+      belongsTo:id
+    }));
+  };
+  
+  return (
+
+    <React.Fragment>
+      <div className="page-content">
+        <div className="container-fluid">
+          <Row>
+            <Col className="col-12">
+              <Card>
+                <CardHeader className="d-flex flex-column gap-3">
+                  <div>  
+                    
+                    
+                    <Dropdown isOpen={searchDropDown} toggle={toggleSearchDropDown} className="d-inline-block">
+                      <DropdownToggle>
+                        <input className="form-control"
+                          autoComplete="off"
+                          onChange={debouncedChangeHandler}    
+                        /> 
+                      </DropdownToggle>
+                      <DropdownMenu className="language-switch dropdown-menu-end">
+                        {props.clients.map(client=> (
+                          <DropdownItem
+                            key={client._id}
+                            onClick={()=>selectClient(client._id)}
+                            
+                          >
+            
+                            <span className="align-middle">
+                              {`${client.firstName} ${client.lastName}`}
+                            </span>
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                    <Dropdown isOpen={menu} toggle={toggle} className="d-inline-block">
+                      <DropdownToggle className="btn header-item " tag="button">
+                        <h2>Wallets</h2>
+                      </DropdownToggle>
+                      <DropdownMenu className="language-switch dropdown-menu-end">
+                        {props.wallets.map(wallet=> (
+                          <DropdownItem
+                            key={wallet._id}
+                            
+                          >
+                            <span className="align-middle">
+                              {wallet.recordId}
+                            </span>
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                    <AddDepositForm/>
+                  </div>
+                </CardHeader>
+                <CardBody>
+              
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+      
+    </React.Fragment>
+    
+  );
+}
+const mapStateToProps = (state)=>({
+  clients:state.clientReducer.clients || [],
+  wallets:state.walletReducer.wallets || []
+}
+);
+export default connect(mapStateToProps, null)(Deposit);
