72e32939747de9106c3afdd134cedfa2a82b4c33
roaaAli97 <roaaa5824@gmail.com>
2022-05-15 13:39:58 +0200

    WIthdrawal page -POR-95

--- src/pages/Transactions/Withdrawal.js
+++ src/pages/Transactions/Withdrawal.js
@@ -1,116 +1,124 @@
 import React, {
-  useState, useEffect, useCallback
+  useState, useEffect
 } from "react";
-import {useDispatch, connect } from "react-redux";
+import { useDispatch, connect } from "react-redux";
 import { fetchClientsStart } from "store/client/actions";
 import { fetchWalletStart } from "store/wallet/action";
 import {
-  Row, Col, Card, CardBody, CardHeader, Dropdown, DropdownMenu, DropdownToggle, DropdownItem
+  Row, Col, Card, CardBody, CardHeader
 } from "reactstrap";
-  
-import { debounce } from "lodash";
+import { AvForm, AvField } from "availity-reactstrap-validation";
 import WithDrawForm from "./WithDrawForm";
-function Deposit(props){
+import { fetchWithdrawalsStart } from "store/transactions/withdrawal/action";
+import SearchBar from "components/Common/SearchBar";
+function Withdrawal(props){
   const dispatch = useDispatch();
   const [searchInput, setSearchInput] = useState("");
-  const [menu, setMenu] = useState(false);
-  const [searchDropDown, setSearchDropDown] = useState(false);
-  const toggleSearchDropDown = ()=>setSearchDropDown(preValue=>!preValue);
-  const toggle = ()=>setMenu(preValue=>!preValue);
+  const [selectedClientId, setSelectedClientId] = useState("");
+  const [selectedWalletId, setSelectedWalletId] = useState("");
+  
+  function selectWallet(id){
+    setSelectedWalletId(id);
+  }
   useEffect(()=>{
-    if (searchInput !== ""){
+    if (searchInput.length >= 3){
       dispatch(fetchClientsStart({
         searchText:searchInput
       }));
     }
-     
+
   }, [searchInput]);
+  useEffect(()=>{
+    dispatch(fetchWithdrawalsStart());
+  }, []);
+  
   const handleSearchInput = (e)=>{
-    
+  
     setSearchInput(e.target.value);
-      
-  };
-  const debouncedChangeHandler = useCallback(
-    debounce(handleSearchInput, 1000), []
-  );
     
+  };
+  
+  
   const selectClient = (id)=>{
-      
+    setSelectedClientId(id);
+    setSelectedWalletId("");
     dispatch(fetchWalletStart({
       belongsTo:id
     }));
+     
   };
-    
-  return (
   
+  return (
+
     <React.Fragment>
       <div className="page-content">
         <div className="container-fluid">
           <Row>
             <Col className="col-12">
               <Card>
-                <CardHeader className="d-flex flex-column gap-3">
-                  <div>  
-                      
-                      
-                    <Dropdown isOpen={searchDropDown} toggle={toggleSearchDropDown} className="d-inline-block">
-                      <DropdownToggle>
-                        <input className="form-control"
-                          autoComplete="off"
-                          onChange={debouncedChangeHandler}    
-                        /> 
-                      </DropdownToggle>
-                      <DropdownMenu className="language-switch dropdown-menu-end">
-                        {props.clients.map(client=> (
-                          <DropdownItem
-                            key={client._id}
-                            onClick={()=>selectClient(client._id)}
-                              
-                          >
-              
-                            <span className="align-middle">
-                              {`${client.firstName} ${client.lastName}`}
-                            </span>
-                          </DropdownItem>
-                        ))}
-                      </DropdownMenu>
-                    </Dropdown>
-                    <Dropdown isOpen={menu} toggle={toggle} className="d-inline-block">
-                      <DropdownToggle className="btn header-item " tag="button">
-                        <h2>Wallets</h2>
-                      </DropdownToggle>
-                      <DropdownMenu className="language-switch dropdown-menu-end">
-                        {props.wallets.map(wallet=> (
-                          <DropdownItem
-                            key={wallet._id}
-                              
-                          >
-                            <span className="align-middle">
-                              {wallet.recordId}
-                            </span>
-                          </DropdownItem>
-                        ))}
-                      </DropdownMenu>
-                    </Dropdown>
-                    <WithDrawForm/>
+                <CardHeader className="d-flex flex-column gap-3 ">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <h1>Withdrawal Page</h1>
+                    <WithDrawForm selectedWallet={selectedWalletId} selectedClient={selectedClientId}/>
                   </div>
+                  
+                  <SearchBar handleSearchInput={handleSearchInput} placeholder="Search for the client"/>
+                      
                 </CardHeader>
-                <CardBody>
+                  
                 
+                <CardBody>
+                  <AvForm>
+                    <Row className="col-12">
+                      <Col md="6">
+                        
+                        <AvField name="clients_list" 
+                          type="select" 
+                          label="Select a client "
+                          onChange={(e)=>selectClient(e.target.value)}>
+                          <option hidden>Select a client</option>
+                          {props.clients.map(client=> (
+                            <option key={client._id} value={client._id} >
+                              {`${client.firstName} ${client.lastName}`}
+                            </option>
+                          ))}
+                        </AvField>
+                      </Col>
+                      <Col md="6">
+                        <AvField name="wallet" 
+                          type="select" 
+                          label="Select a wallet"
+                          id="walletList"
+                          onChange={(e)=>selectWallet(e.target.value)}>
+                          <option hidden>Select a wallet</option>
+                          {props.wallets.map(wallet=> (
+                            <option key={wallet._id} value={wallet._id} >
+                              {wallet.recordId}
+                            </option>
+                          ))}
+                     
+                        </AvField>
+                      </Col> 
+                      
+                    </Row>
+                  </AvForm>
+            
+           
                 </CardBody>
               </Card>
             </Col>
           </Row>
         </div>
       </div>
-        
-    </React.Fragment>
       
+    </React.Fragment>
+    
   );
 }
 const mapStateToProps = (state)=>({
   clients:state.clientReducer.clients || [],
-  wallets:state.walletReducer.wallets || []
+  wallets:state.walletReducer.wallets || [],
+  withdrawals:state.withdrawalReducer.withdrawals || []
 }
 );
-export default connect(mapStateToProps, null)(Deposit);
+export default connect(mapStateToProps, null)(Withdrawal);
