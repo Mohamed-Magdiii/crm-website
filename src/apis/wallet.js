05b794511e96708f1b4f18252379720f647bfd38
roaaAli97 <roaaa5824@gmail.com>
2022-05-15 13:38:34 +0200

    wallet api calls - POR-95

--- src/apis/wallet.js
+++ src/apis/wallet.js
@@ -3,6 +3,5 @@ import * as axiosHelper from "./api_helper";
 
 export const getWallets = async  ({ payload })=>{
   const wallets = await axiosHelper.get(`/wallets?${qs.stringify(payload)}`, { crypto:true });
-  console.log(wallets);
   return wallets;
 };
