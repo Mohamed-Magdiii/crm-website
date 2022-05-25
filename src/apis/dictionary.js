259b294c7216a60255575bc9ffb444463008e003
roaaAli97 <roaaa5824@gmail.com>
2022-05-25 09:53:59 +0200

    Exchanges List -POR-182

--- src/apis/dictionary.js
+++ src/apis/dictionary.js
@@ -8,3 +8,43 @@ export const getDictionary = async()=>{
   }
  
 };
+export const updateEmailProvider = async ({ value, body })=>{
+  try {
+    const result = await axiosHelper.patch(`/dictionaries/emailProviders/${value}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened during updating this email provider");
+  }
+};
+export const updateActions = async ({ value, body }) =>{
+  try {
+    const result = await axiosHelper.patch(`/dictionaries/actions/${value}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened during updating this action");
+  }
+};
+export const updateCountries = async ({ value, body })=>{
+  try {
+    const result = await axiosHelper.patch(`/dictionaries/countries/${value}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened during updating this action");
+  }
+};
+export  const addNewItem = async({ id, body }) =>{
+  try {
+    const result = await axiosHelper.patch(`/dictionaries/addItem/${id}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened during adding this item");
+  }
+};
+export const removeItem = async ({id, body})=>{
+  try {
+    const result = await axiosHelper(`/dictionaries/removeItem/${id}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened while removing this item ");
+  }
+};
---
+++ src/pages/Dictionary.js/Exchanges/ExchangesTab.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {useDispatch, connect} from "react-redux";
+import {
+  CardBody, CardHeader, Card, CardTitle
+} from "reactstrap";
+import TableLoader from "components/Common/TableLoader";
+import {
+  Table, Thead, Tbody, Tr, Th, Td
+} from "react-super-responsive-table";
+import { Link } from "react-router-dom";
+import ExchangeAddModal from "./ExchangeAddModal";
+function ExchangesTab(props){
+  
+  
+  return (
+    <React.Fragment>
+      <Card>
+        <CardHeader className="d-flex flex-column gap-3">
+          <div className="d-flex justify-content-between  align-items-center">
+            <CardTitle>Exchanges</CardTitle>
+            <ExchangeAddModal />
+          </div>
+        </CardHeader>
+        <CardBody>
+          <div className="table-rep-plugin">
+            <div
+              className="table-responsive mb-0"
+              data-pattern="priority-columns"
+            >
+              <Table
+                id="tech-companies-1"
+                className="table "
+              >
+                <Thead>
+                  <Tr>
+                    <Th>Exchanges</Th>
+                    <Th>Actions</Th>
+                  </Tr>
+                </Thead>
+                {props.dictionary[0] ? <Tbody>
+                  {props.loading && <TableLoader colSpan={4} />}
+                  {!props.loading && props.dictionary[0].exchanges.map((row, rowIndex) =>
+                    <Tr key={rowIndex}>
+                      <Td>{row}</Td>
+                      <Td>
+                        <div className="d-flex gap-3">
+                          <Link className="text-success" to="#">
+                            <i
+                              className="mdi mdi-pencil font-size-18"
+                              id="edittooltip"
+                          
+                            ></i>
+                          </Link>
+                        </div>
+                      </Td>
+                    </Tr>
+                  
+                  )}
+                </Tbody> : <Tbody></Tbody>
+                }
+              </Table>
+           
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+         
+    </React.Fragment>
+  );
+}
+const mapStateToProps = (state)=>({
+  dictionary:state.dictionaryReducer.dictionary || []
+});
+export default connect(mapStateToProps, null)(ExchangesTab);
