95f9982280fe9d45e45e7f61182228e0b54a1080
roaaAli97 <roaaa5824@gmail.com>
2022-05-25 09:55:22 +0200

    Countries List-POR-182

---
+++ src/pages/Dictionary.js/Countries/CountriesTab.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import {useDispatch, connect} from "react-redux";
+import {
+  CardBody, Card, CardTitle, CardHeader
+} from "reactstrap";
+import TableLoader from "components/Common/TableLoader";
+import {
+  Table, Thead, Tbody, Tr, Th, Td
+} from "react-super-responsive-table";
+import { Link } from "react-router-dom";
+import CountriesAdd from "./CountriesAdd";
+function CountriesTab(props){
+  return (
+    <React.Fragment>
+      <Card>
+        <CardHeader className="d-flex flex-column gap-3">
+          <div className="d-flex justify-content-between  align-items-center">
+            <CardTitle>Countries</CardTitle>
+            <CountriesAdd/>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <div className="table-rep-plugin">
+            <div
+              className="table-responsive mb-0"
+              data-pattern="priority-columns"
+            >
+              <Table
+                id="tech-companies-1"
+                className="table "
+              >
+                <Thead>
+                  <Tr>
+                    <Th>Alpha2</Th>
+                    <Th>Alpha2</Th>
+                    <Th>Calling Code</Th>
+                    <Th>AR</Th>
+                    <Th>EN</Th>
+                    <Th>Actions</Th>
+                  </Tr>
+                </Thead>
+                {props.dictionary[0] ? <Tbody>
+                  {props.loading && <TableLoader colSpan={4} />}
+                  {!props.loading && props.dictionary[0].countries.map((row, rowIndex) =>
+                    <Tr key={rowIndex}>
+                      <Td>{row.alpha2}</Td>
+                      <Td>{row.alpha3}</Td>
+                      <Td>{row.callingCode}</Td>
+                      <Td>{row.ar}</Td>
+                      <Td>{row.en}</Td>
+                      <Td>
+                        <div className="d-flex gap-3">
+                          <Link className="text-success" to="#">
+                            <i
+                              className="mdi mdi-pencil font-size-18"
+                              id="edittooltip"
+                  
+                            ></i>
+                          </Link>
+                        </div>
+                      </Td>
+                    </Tr>
+          
+                  )}
+                </Tbody> : <Tbody></Tbody>
+                }
+              </Table>
+   
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </React.Fragment>
+  );
+}
+const mapStateToProps = (state)=>({
+  dictionary:state.dictionaryReducer.dictionary || []
+});
+export default connect(mapStateToProps, null)(CountriesTab);
