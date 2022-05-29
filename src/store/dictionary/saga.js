0694464ecff9df0a80d48c10862ae89a8025bd95
roaaAli97 <roaaa5824@gmail.com>
2022-05-29 13:20:55 +0200

    calling updatAction in the saga-POR-182

--- src/store/dictionary/saga.js
+++ src/store/dictionary/saga.js
@@ -2,7 +2,7 @@ import {
   call, put, takeEvery
 } from "redux-saga/effects";
 import { 
-  getDictionary, addNewItem, removeItem 
+  getDictionary, addNewItem, removeItem, updateActions, updateEmailProvider, updateCountries, updateExchange
 } from "apis/dictionary";
 import { 
   fetchDictionarySuccess,
@@ -21,6 +21,10 @@ import {
   FETCH_DICTIONARY_START,
   ADD_NEW_ITEM, 
   REMOVE_ITEM, 
+  UPDATE_ACTION_START,
+  UPATE_EMAIL_PROVIDER_START,
+  UPDATE_EXCHANGE_START,
+  UPDATE_COUNTRY_START
 } from "./actionsType";
 function * fetchDictionary(){
   try {
@@ -59,6 +63,15 @@ function * addItem({ payload }){
   
   
 }
+function * updateAction({ payload }){
+  console.log(payload);
+  try {
+    const result = yield call(updateActions, payload);
+    console.log(result);
+  } catch (error){
+    yield put(apiError(error));
+  }
+}
 function * removeItemFromDictionary({ payload }){
   try {
     
@@ -92,6 +105,7 @@ function * dictionarySaga (){
   yield takeEvery(FETCH_DICTIONARY_START, fetchDictionary);
   yield takeEvery(ADD_NEW_ITEM, addItem);
   yield takeEvery(REMOVE_ITEM, removeItemFromDictionary);
-  
+  yield takeEvery (UPDATE_ACTION_START, updateAction);
+
 }
 export default dictionarySaga;
