09b1bf766df6a18cc291aeb70d508567c20699c1
roaaAli97 <roaaa5824@gmail.com>
2022-05-29 12:27:07 +0200

    Edit Form for Countries -POR-182

---
+++ src/pages/Dictionary.js/Countries/CountriesEdit.js
@@ -0,0 +1,105 @@
+import { useDispatch, connect } from "react-redux";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  UncontrolledAlert,
+  Col,
+  Row
+} from "reactstrap";
+import { withTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import { addNewItem } from "store/dictionary/actions";
+function CountriesEdit(props){
+  const {open, onClose, country} = props;
+  const dispatch = useDispatch();
+  
+  return (
+    <React.Fragment >
+      
+      <Modal isOpen={open} toggle={onClose} centered={true}>
+        <ModalHeader toggle={onClose} tag="h4">
+          Edit Country
+        </ModalHeader>
+        <ModalBody >
+          <AvForm
+            className='p-4'
+            onValidSubmit={(e, v) => {
+              e.preventDefault();
+            }}
+          >
+            
+            
+            <div className="mb-3">
+              <AvField
+                name="alpha2"
+                label="Alpha2"
+                placeholder="Alpha2"
+                type="text"
+                errorMessage="Enter valid alpha2"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            <div className="mb-3">
+              <AvField
+                name="alpha3"
+                label="Alpha3"
+                placeholder="Alpha3"
+                type="text"
+                errorMessage="Enter valid alpha3"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            <div className="mb-3">
+              <AvField
+                name="callingCode"
+                label="Calling Code"
+                placeholder="Calling Code"
+                type="text"
+                errorMessage="Enter valid calling code"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            <div className="mb-3">
+              <AvField
+                name="ar"
+                label="Ar"
+                placeholder="AR"
+                type="text"
+                errorMessage="Enter valid ar"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            <div className="mb-3">
+              <AvField
+                name="en"
+                label="EN"
+                placeholder="en"
+                type="text"
+                errorMessage="Enter valid en"
+                validate={{ required: { value: true } }}
+              />
+            </div>
+            
+            <div className='text-center pt-3 p-2'>
+              <Button  type="submit" color="primary" className="">
+                     Edit Country
+              </Button>
+            </div>
+          </AvForm>
+        </ModalBody>
+      </Modal>
+    </React.Fragment>
+  );
+}
+const mapStateToProps = (state)=>({
+  loading: state.dictionaryReducer.loading || false,
+  dictionary: state.dictionaryReducer.dictionary || [],
+  error : state.dictionaryReducer.error,
+  actions :state.dictionaryReducer.actions || [],
+  id :state.dictionaryReducer.id
+});
+export default connect(mapStateToProps, null)(CountriesEdit);
