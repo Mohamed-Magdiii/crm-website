25f8016640b59bef9612b827d5cb3f3604ed848a
roaaAli97 <roaaa5824@gmail.com>
2022-05-30 17:46:48 +0200

    api call for fee groups -POR-218

---
+++ src/apis/fee-groups.js
@@ -0,0 +1,37 @@
+import * as axioHelper from "./api_helper";
+import qs from "qs";
+
+export async function fetchFeeGroups ({ payload }){
+  try {
+    const result = await axioHelper.get(`/fee-groups?${qs.stringify(payload)}`);
+    return result;
+  } catch (error){
+    throw new Error("Error happened while fetching data");
+  }
+   
+
+}
+export async function updateFeeGroup ({ body, id }){
+  try {
+    const result = await axioHelper.patch(`/fee-groups/${id}`, body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened while updating data");
+  }
+}
+export async function addFeeGroup ({ body }){
+  try {
+    const result = await axioHelper.post("/fee-groups", body);
+    return result;
+  } catch (error){
+    throw new Error("Error happened while adding data");
+  }
+}
+export async function deleteFeeGroup ({ id }){
+  try {
+    const result = await axioHelper.del(`/fee-groups/${id}`);
+    return result;
+  } catch (error){
+    throw new Error("Error happened while deleting data");
+  }
+}
