94638d1d6f91c83ad6fd0c883209eec58a8a4be4
abdelrhmanshokr <526473+gb96@users.noreply.github.com>
2022-06-02 18:57:49 +0200

    done with most of the required updates except the client details page

--- src/pages/Client/ClientDetails.js
+++ src/pages/Client/ClientDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, connect } from "react-redux";
 import {
-  Row, Col, Button, UncontrolledAlert
+  Row, Col, Button, UncontrolledAlert, CardBody, CardHeader, CardTitle
 } from "reactstrap";
 import {
   AvForm, AvField
@@ -13,6 +13,7 @@ import { fetchClientDetails, editClientDetails } from "store/client/actions";
 
 function ClientDetails(props) {
   const clientId = props.clientId;
+  const titles = ["Mr", "Mrs", "Miss", "Ms", "Dr"];
   const dispatch = useDispatch();
   const loadClientDetails = () => {
     dispatch(fetchClientDetails(clientId)); 
@@ -36,12 +37,17 @@ function ClientDetails(props) {
           <Row>
             {/* form on the left side */}
             <Col className="col-10"> 
-              <AvForm
-                onValidSubmit={(e, v) => {
-                  loadUpdatedClientDetailsHandler(e, v);
-                }}
-              >
-                <div className="">
+              <CardHeader className="d-flex flex-column gap-3">
+                <div className="d-flex justify-content-between  align-items-center">
+                  <CardTitle>{props.t("General information")}</CardTitle>
+                </div>
+              </CardHeader>
+              <CardBody>
+                <AvForm
+                  onValidSubmit={(e, v) => {
+                    loadUpdatedClientDetailsHandler(e, v);
+                  }}
+                >
                   {/* first row */}
                   <div className="input-group">
                     {/* first name */}
@@ -58,6 +64,7 @@ function ClientDetails(props) {
 
                     {/* last name */}
                     <AvField
+                      className="group-control"
                       name="lastName"
                       label={props.t("Last name")}
                       placeholder={props.t("Last name")}
@@ -72,17 +79,25 @@ function ClientDetails(props) {
                   <div className="input-group">
                     {/* title */}
                     <AvField
+                      className="group-control"
                       name="title"
                       label={props.t("Title")}
                       placeholder={props.t("Title")}
-                      type="text"
+                      type="select"
                       errorMessage={props.t("Title is required")}
                       validate={{ required: { value: true } }}
                       value={props.clientDetails.title}
-                    />
+                    >
+                      {titles.map((title) => (
+                        <option key={title.indexOf(title)} value={title}>
+                          title
+                        </option>
+                      ))}
+                    </AvField>
                     
                     {/* phone number */}
                     <AvField
+                      className="group-control"
                       name="phone"
                       label={props.t("Phone")}
                       placeholder={props.t("Phone")}
@@ -93,16 +108,11 @@ function ClientDetails(props) {
                     />
                   </div>
 
-                  <div className="col-sm-3">
-                  </div>
-                  <div className="col-sm-3">
-                  </div>
-                  <div className="col-sm-3">
-                  </div>
-
-                  {/* nationality */}
-                  <div className="col-sm-8">
+                  {/* third row */}
+                  <div className="input-group">
+                    {/* nationality */}
                     <AvField
+                      className="group-control"
                       name="nationality"
                       label={props.t("Nationality")}
                       placeholder={props.t("Nationality")}
@@ -111,15 +121,8 @@ function ClientDetails(props) {
                       validate={{ required: { value: true } }}
                       value={props.clientDetails.nationality}
                     />
-                  </div>
-
-                  {/* phone */}
-                  <div className="col-sm-8">
                     
-                  </div>
-
-                  {/* country */}
-                  <div className="col-sm-8">
+                    {/* country */}
                     <AvField
                       name="country"
                       label={props.t("Country")}
@@ -131,8 +134,9 @@ function ClientDetails(props) {
                     />
                   </div>
 
-                  {/* city */}
-                  <div className="col-sm-8">
+                  {/* forth row */}
+                  <div className="input-group">
+                    {/* city */}
                     <AvField
                       name="city"
                       label={props.t("City")}
@@ -142,10 +146,8 @@ function ClientDetails(props) {
                       validate={{ required: { value: true } }}
                       value={props.clientDetails.city}
                     />
-                  </div>
-
-                  {/* agent */}
-                  <div className="col-sm-8">
+                    
+                    {/* agent */}
                     <AvField
                       name="agent"
                       label={props.t("Agent")}
@@ -157,8 +159,9 @@ function ClientDetails(props) {
                     />
                   </div>
 
-                  {/* call status */}
-                  <div className="col-sm-8">
+                  {/* final row */}
+                  <div className="input-group">
+                    {/* call status */}
                     <AvField
                       name="callStatus"
                       label={props.t("Call status")}
@@ -168,10 +171,8 @@ function ClientDetails(props) {
                       validate={{ required: { value: true } }}
                       value={props.clientDetails.callStatus}
                     />
-                  </div>
-
-                  {/* dob */}
-                  <div className="col-sm-8">
+                    
+                    {/* dob */}
                     <AvField
                       name="dob"
                       label={props.t("Dob")}
@@ -183,7 +184,7 @@ function ClientDetails(props) {
                     />
                   </div>
 
-                  <div className="d-flex justify-content-end col-sm-8 mt-3">
+                  <div className="d-flex justify-content-end col-sm-6 mt-3">
                     {/* submit button */}
                     <div className="p-2">
                       <Button 
@@ -195,20 +196,30 @@ function ClientDetails(props) {
                       </Button>
                     </div>
                   </div>
-                </div>
-                {/* TODO this error message needs to be handled to disappear after a while */}
-                {props.editError && <UncontrolledAlert color="danger" className="col-sm-8">
-                  <i className="mdi mdi-block-helper me-2"></i>
-                  {/* TODO this need to be handled in translation */}
-                  {props.t(props.editErrorDetails)}
-                </UncontrolledAlert>}
-                {props.editSuccess && <UncontrolledAlert color="success" className="col-sm-8">
-                  <i className="mdi mdi-check-all me-2"></i>
-                  {props.t("Client updated successfully")} !!!
-                </UncontrolledAlert>}
-              </AvForm>
+                  {/* TODO this error message needs to be handled to disappear after a while */}
+                  {props.editError && <UncontrolledAlert color="danger" className="col-sm-8">
+                    <i className="mdi mdi-block-helper me-2"></i>
+                    {/* TODO this need to be handled in translation */}
+                    {props.t(props.editErrorDetails)}
+                  </UncontrolledAlert>}
+                  {props.editSuccess && <UncontrolledAlert color="success" className="col-sm-8">
+                    <i className="mdi mdi-check-all me-2"></i>
+                    {props.t("Client updated successfully")} !!!
+                  </UncontrolledAlert>}
+                </AvForm>
+              </CardBody>
             </Col>
             {/* card on the right side */}
+            <Col>
+              <CardHeader className="d-flex flex-column gap-3">
+                <div className="d-flex justify-content-between  align-items-center">
+                  
+                </div>
+              </CardHeader>
+              <CardBody>
+                testing
+              </CardBody>
+            </Col>
           </Row>
         </div>
       </div>
