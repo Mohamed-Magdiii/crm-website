005d131320e93777f60750c1f693f888c21133d8
muhammad-aadil-1 <aadil@exiniti.com>
2022-06-06 13:16:58 +0400

    fixes for eslint

--- src/pages/Roles/RolesEdit.js
+++ src/pages/Roles/RolesEdit.js
@@ -33,7 +33,7 @@ function RolesAdd (props) {
   return (
     <React.Fragment >
       {/* <Link to="#" className="btn btn-light" onClick={onClose}><i className="bx bx-plus me-1"></i> Add New</Link> */}
-      <Modal isOpen={open} toggle={onClose} centered={true}>
+      <Modal isOpen={open} toggle={onClose} centered={true} size={"lg"}>
         <ModalHeader toggle={onClose} tag="h4">
             Edit Role
         </ModalHeader>
