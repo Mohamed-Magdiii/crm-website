b4e4e82bacb87197413c09c8de4c056e5a958fa3
roaaAli97 <roaaa5824@gmail.com>
2022-06-07 17:05:28 +0200

    adding validation to api call -POR-223

--- src/apis/profile.js
+++ src/apis/profile.js
@@ -1,6 +1,12 @@
 import * as axiosHelper from "./api_helper";
 
 export async function getProfile(){
+  
   const result = await axiosHelper.get("/auth/profile");
+  if (result.code === 401){
+    throw new Error ("You are not authorized");
+  }
   return result;
+
+
 }
