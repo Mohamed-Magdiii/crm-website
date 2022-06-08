776f3e443f2d121d84ab3dff8890695e733fd2f8
mostafa-sultan <mostafasoltan82@gmail.com>
2022-06-08 14:34:44 +0200

    get price api call POR-217

--- src/apis/orders.js
+++ src/apis/orders.js
@@ -3,28 +3,28 @@ import * as axiosHelper from "./api_helper";
 
 export const getOrders = async ({ payload }) => {
   const data = await axiosHelper.get(`/order/?${qs.stringify(payload)}`);
-  if (data.isError){
+  if (data.isError) {
     throw new Error(data.mesage);
   }
-    
+
   return data.result;
 };
 
 export const getUserOrders = async ({ payload }) => {
   const id = payload;
   const data = await axiosHelper.get(`/order/${id}`);
-  if (data.isError){
+  if (data.isError) {
     throw new Error(data.message);
   }
 
   return data;
 };
 
-export const addUserOrder = async({ payload }) => {
+export const addUserOrder = async ({ payload }) => {
   const data = await axiosHelper.post("/order", payload);
   if (data.isError) {
     throw new Error(data.message);
-  } 
+  }
 
   return data;
 };
@@ -33,10 +33,14 @@ export const getMarkets = async ({ payload }) => {
   const data = await axiosHelper.get(`/markets?${qs.stringify(payload)}`);
   return data.result;
 };
+export const getPricing = async (payload) => {
+  const data = await axiosHelper.get("/pricing?" + ("pairName=" + payload.pairName) + ("&markupId=" + (payload?.markupId || "")));
+  return data.result;
+};
 export const deleteUserOrder = async ({ payload }) => {
   // here the payload is just the id for system email to be deleted 
   const data = await axiosHelper.del(`/order/${payload}`);
-  if (data.isError){
+  if (data.isError) {
     throw new Error(data.message);
   }
   return data;
