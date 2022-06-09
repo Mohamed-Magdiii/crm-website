b0ad99adcae2adf1ea9407c1bfcf9aec53ee325c
roaaAli97 <roaaa5824@gmail.com>
2022-06-09 17:13:45 +0200

    getting countries data from dictionaries -POR-227

--- src/components/Common/CountryDropDown.js
+++ src/components/Common/CountryDropDown.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { fetchDictionaryStart } from "store/dictionary/actions";
 import { useDispatch, connect } from "react-redux";
 import Select from "react-select";
@@ -12,18 +12,17 @@ function CountryDropDown({ selectCountry, ...props }){
   
   const optionGroup = props.countries.map(country=>{
     return {
-      label: country.ar, 
-      value: country.ar
+      label: `${country.en} ${country.ar}`, 
+      value: country.en
     };
   });
   
   
   return (
     <React.Fragment>
-    
-  
+
       <div className="mb-3">
-        <label htmlFor="choices-single-default" className="form-label font-size-13 text-muted">Country</label>
+        <label htmlFor="choices-single-default" className="form-label font-size-14">Country</label>
         <Select 
           onChange={(e) => {
             selectCountry(e.value);
@@ -31,9 +30,10 @@ function CountryDropDown({ selectCountry, ...props }){
           
           options={optionGroup}
           classNamePrefix="select2-selection"
+          placeholder = "Select Your Country"
+          
         />
       </div>
-        
     </React.Fragment>);
 
 }
