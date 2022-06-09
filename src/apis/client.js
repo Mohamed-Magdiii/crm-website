0fab62ef5ef5e012b59d7f1645d3a69e76458559
roaaAli97 <roaaa5824@gmail.com>
2022-06-09 17:13:11 +0200

    fixing some apis calls -POR-227

--- src/apis/client.js
+++ src/apis/client.js
@@ -6,11 +6,8 @@ export const getClients = async ({ payload }) => {
   return data;
 };
 
-export const addClient = async (values) => {
-  
-    
+export const addClient = async (values) => {  
   const data = await axiosHelper.post("/clients", { ...values });
-  console.log(data);
   if (data.isSuccess === false){
     throw new Error("Invalid data");
   }
