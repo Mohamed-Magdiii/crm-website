d1c74e95e9665f81c08ca97c550c3f4e16cca7e6
roaaAli97 <roaaa5824@gmail.com>
2022-06-26 09:56:29 +0200

    adding details modal that shows the details of blockchain transaction

---
+++ src/pages/Transactions/DetailsModal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody
+} from "reactstrap";
+import { captilazeFirstLetter } from "common/utils/manipulateString";
+import { withTranslation } from "react-i18next";
+function DetailsModal(props){
+  const { rawData, open, onClose } = props;
+  
+  return (
+    <React.Fragment>
+      <div>
+        <Modal isOpen= {open} toggle = {onClose} centered = {true} size = {"lg"}>
+          <ModalHeader toggle = {onClose} tag="h4">
+            {props.t("Details")}
+          </ModalHeader>
+          <ModalBody>
+            <div >
+              {rawData ? Object.keys(rawData).map(key => {
+                return <p key={rawData[key]}>{`${captilazeFirstLetter(props.t(key))} :${props.t(rawData[key])}`}</p>;
+              }) : "" }
+            </div>
+          </ModalBody>
+        </Modal>
+      </div>
+    </React.Fragment>
+  );
+}
+export default withTranslation()(DetailsModal);
