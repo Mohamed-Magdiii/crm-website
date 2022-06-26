77aa024247d991f4b1d8b6d9ced6b54096cf95a5
mostafa-sultan <mostafasoltan82@gmail.com>
2022-06-26 15:28:11 +0200

    CB-186

--- src/pages/Users/UsersAddModal.js
+++ src/pages/Users/UsersAddModal.js
@@ -43,7 +43,7 @@ function UsersAddModal(props) {
 
   return (
     <React.Fragment >
-      <Link to="#" className={`btn btn-primary ${!create ? "d-none" : ""}`} onClick={toggleAddModal}><i className="bx bx-plus me-1"></i> Add New</Link>
+      <Link to="#" className={`btn btn-primary ${!create ? "d-none" : ""}`} onClick={toggleAddModal}><i className="bx bx-plus me-1"></i> Add New User</Link>
       <Modal isOpen={addModal} toggle={toggleAddModal} centered={true}>
         <ModalHeader toggle={toggleAddModal} tag="h4">
           Add New User
