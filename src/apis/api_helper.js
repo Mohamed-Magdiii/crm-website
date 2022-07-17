189baa7d871b6eefe6d0ceae6c00eb47c5d0c1f9
roaaAli97 <roaaa5824@gmail.com>
2022-07-17 17:51:35 +0200

    using local url

--- src/apis/api_helper.js
+++ src/apis/api_helper.js
@@ -17,8 +17,7 @@ export const redirectToLogin = function () {
 };
 
 //apply base url for axios
-const API_URL = "http://localhost:3001/api/v1/";
-
+const API_URL = `${process.env.REACT_APP_API_CRM_DOMAIN}/api/v1/`;
 const axiosApi = axios.create({
   baseURL: API_URL,
 });
