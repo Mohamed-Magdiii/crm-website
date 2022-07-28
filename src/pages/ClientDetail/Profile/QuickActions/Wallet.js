fce4849323e4c6fe1a7a4adda8f42ff551f37756
roaaAli97 <roaaa5824@gmail.com>
2022-07-28 14:11:43 +0200

    setting up convert wallet modal and dispatching an action-POR-248

--- src/pages/ClientDetail/Profile/QuickActions/Wallet.js
+++ src/pages/ClientDetail/Profile/QuickActions/Wallet.js
@@ -5,19 +5,35 @@ import {
   Button,
   ModalHeader,
   ModalBody,
+  Label,
+  UncontrolledAlert
 } from "reactstrap";
 import { useDispatch, connect } from "react-redux";
-import AvFieldSelect from "components/Common/AvFieldSelect";
-import { AvForm } from "availity-reactstrap-validation";
-import { fetchClientWallets } from "store/wallet/action";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import { fetchClientWallets, convertWalletStart } from "store/wallet/action";
 import { fetchAssetsStart } from "store/assests/actions";
+
+import Select from "react-select";
 function ConvertWallet(props){
   const [addModal, setAddModal] = useState(false);
+  const [toAsset, setToAsset] = useState("");
+  const [fromAsset, setFromAsset] = useState("");
+  const [toAssetId, setToAssetId ] = useState("");
+  const [fromAssetId, setFromAssetId] = useState("");
   const { clientId } = props;
   const toggleAddModal = ()=>{
     setAddModal(!addModal);
   };
   const dispatch = useDispatch();
+  const handleConvertWallet = (e, v)=>{
+    dispatch(convertWalletStart({
+      toAsset,
+      fromAsset,
+      fromAssetId,
+      toAssetId,
+      amount:v.amount
+    }));
+  };
   useEffect(()=>{
     dispatch(fetchClientWallets(clientId));
     dispatch(fetchAssetsStart({
@@ -38,35 +54,62 @@ function ConvertWallet(props){
           <AvForm
             className="p-4"
             onValidSubmit={ (e, v) => {
-          
+              handleConvertWallet(e, v);
             }}
           >
-            <AvFieldSelect
+            <Label>From Asset</Label>
+            <Select
               type="text" 
-              label = "Client Wallets"
-              name="oldAsset"
+              label = "From Asset"
+              name="toAsset"
               options = {props.docs && props.docs.map((wallet)=>{
                 return {
-                  label:wallet.asset,
-                  value:wallet._id
+                  label:`${wallet.asset} (${wallet.amount})`,
+                  value:{
+                    id:wallet._id,
+                    asset:wallet.asset
+                  }
                 };
-              })}/>
-            <AvFieldSelect 
+              })}
+              onChange= {e=>{
+                setFromAsset(e.value.asset);
+                setFromAssetId(e.value.id);
+              }}
+            />
+            <Label>To Asset</Label>
+            <Select 
               
               type="text" 
-              label="New Asset" 
-              name="newAsset"
-              options = {props.assets.map(asset=>{
+              label="To Asset" 
+              name="fromAsset"
+              options = {props.docs && props.docs.map(wallet=>{
                 return {
-                  label:asset.name,
-                  value: asset.name
+                  label:`${wallet.asset} (${wallet.amount})`,
+                  value: {
+                    id:wallet._id, 
+                    asset:wallet.asset
+                  }
                 };
-              })}/>
+                
+              })}
+              onChange={e=>{
+                setToAssetId(e.value.id);
+                setToAsset(e.value.asset);
+              }}
+            />
+            <AvField 
+              label="Amount"
+              name="amount"
+              type="number"/>
             <div className="mt-2 m-auto">
               <Button className="btn btn-primary">Convert Wallet</Button>
             </div>
            
           </AvForm>
+          {props.convertWalletError && <UncontrolledAlert color="danger">
+            <i className="mdi mdi-block-helper me-2"></i>
+            {props.convertWalletError}
+          </UncontrolledAlert>}
         </ModalBody>
       </Modal>
     </React.Fragment>
@@ -74,6 +117,8 @@ function ConvertWallet(props){
 }
 const mapStateToProps = (state)=>({
   docs: state.walletReducer.docs || [],
-  assets: state.assetReducer.assets || []
+  assets: state.assetReducer.assets || [],
+  convertWalletError : state.walletReducer.convertWalletError,
+  disableConvertWalletButton:state.walletReducer.disableConvertWalletButton
 });
 export default connect(mapStateToProps, null)(ConvertWallet);
