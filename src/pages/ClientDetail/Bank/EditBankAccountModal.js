3cd5a565110c6efe25aa66bbb374e1b9833bf127
abdelrhmanshokr <37106889+abdelrhmanshokr@users.noreply.github.com>
2022-08-04 16:37:23 +0200

    adding new validation for sift code

--- src/pages/ClientDetail/Bank/EditBankAccountModal.js
+++ src/pages/ClientDetail/Bank/EditBankAccountModal.js
@@ -73,11 +73,15 @@ function BankAccountEditModal(props){
                     },
                     minLength: {
                       value: 8,
-                      errorMessage: "Invalid swift code"
+                      errorMessage: "Swift code must consist of 8 characters"
+                    },
+                    maxLength: {
+                      value: 8,
+                      errorMessage: "Swift code must consist of 8 characters"
                     },
                     pattern: {
-                      value: "([A-Z][0-9])",
-                      errorMessage: "Invalid swift code"
+                      value: "/^[A-Z]*$/",
+                      errorMessage: "Swift code can only contain uppercase characters"
                     }
                   }
                 }
@@ -100,11 +104,11 @@ function BankAccountEditModal(props){
                     },
                     pattern: {
                       value: "^[A-Z][A-Z]", 
-                      errorMessage: "Invalid IBAN"
+                      errorMessage: "IBAN must start with two uppercase charecters"
                     },
                     minLength: {
                       value: 13,
-                      errorMessage: "Invalid IBAN"
+                      errorMessage: "IBAN must contain at least 13 characters"
                     }
                   }
                 }
@@ -127,7 +131,7 @@ function BankAccountEditModal(props){
                     },
                     number: {
                       value: true,
-                      errorMessage: "Invalid account number"
+                      errorMessage: "Account number must be a number"
                     }
                   }
                 }
