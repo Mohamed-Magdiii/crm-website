5a89503c636a314765fbf3474ed7d81bd617f0fe
A1Exi1 <a.hany@exinitic.com>
2022-10-17 18:49:34 +0200

    security table

--- src/pages/ClientDetail/Security/Security.js
+++ src/pages/ClientDetail/Security/Security.js
@@ -1,25 +1,99 @@
+import TableLoader from "components/Common/Loader";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Spinner } from "reactstrap";
+import {
+  Tbody, Td, Th, Thead, Tr 
+} from "react-super-responsive-table";
+import {
+  Button, DropdownItem, DropdownMenu, DropdownToggle, Spinner, Table, UncontrolledDropdown 
+} from "reactstrap";
 import { disable2FA } from "store/client/actions";
 
 function Security(props) {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const { loading } = useSelector(state=>state.clientReducer.disable2FA);
+  const { loading, success } = useSelector(state=>state.clientReducer.disable2FA);
   const state = useSelector(state=>state.clientReducer.clientDetails);
   const disable2fa = ()=>{
     dispatch(disable2FA({ id:props.clientId }));
   };
+  const columns = [
+    {
+      text:"Name",
+      dataField:"name"
+    },
+    {
+      text:"Status",
+      dataField:"status",
+      formatter:(val)=> success ? <i className={"bx bx-x-circle"} style={{ fontSize: "1.5rem" }}></i> : <i className={`bx ${val.status ? "bx-check-circle" : "bx-x-circle"}`} style={{ fontSize: "1.5rem" }}></i>
+    },
+    {
+      text:"Actions",
+      dataField:"actions",
+      formatter:(val) => {
+        return val.actions.map((action, index)=> <UncontrolledDropdown key={index}>
+          <DropdownToggle className="text-muted font-size-24" style={{ cursor:"pointer" }} tag="a">
+            <i className="mdi mdi-dots-vertical"></i>
+          </DropdownToggle>
+
+          <DropdownMenu style={{
+            zIndex:"5",
+            position:"relative" 
+          }}>
+            <DropdownItem to="#" disabled={action.disabled} onClick={action.onClick}>{action.title}</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>);
+      }
+    }
+  ];
+  const tabs = [
+    {
+      name:"2FA",
+      status: state?.settings?.twoFactorAuthEnabled,
+      actions: [{
+        title:"Disable 2FA",
+        disabled: state?.settings?.twoFactorAuthEnabled ? false : true,
+        onClick: ()=>{
+          disable2fa();
+        },
+        loading: loading
+      }]
+    },
+  ];
 
   return (<>
-    {state?.settings?.twoFactorAuthEnabled && <div className="d-flex align-items-center justify-content-center"> <Button color="primary" disabled={loading} onClick={disable2fa}>
-      {loading ? <div className="d-flex align-items-center justify-content-center">
-        <Spinner></Spinner>
-      </div> : t("Disable 2FA")}
-    </Button>
-    </div>}
+    <div className="table-rep-plugin">
+      <div
+        className="table-responsive mb-0"
+        data-pattern="priority-columns"
+      >
+        <Table
+          id="tech-companies-1"
+          className="table"
+        >
+          <Thead className="text-center table-light" >
+            <Tr>
+              {columns.map((column, index) =>
+                <Th data-priority={index} key={index}>{column.text}</Th>
+              )}
+            </Tr>
+          </Thead>
+          <Tbody className="text-center" style={{ fontSize: "13px" }}>
+            {props.loading && <TableLoader colSpan={4} />}
+            {!props.loading && tabs.map((row, rowIndex) =>
+              <tr key={rowIndex} >
+                {columns.map((column, index) =>
+                  <td key={`${rowIndex}-${index}`}>                     
+                    {column.formatter ? column.formatter(row, rowIndex) : row[column.dataField]}
+                  </td>
+                )}
+              </tr>
+            )}
+          </Tbody>
+        </Table>
+      </div>
+    </div>
   </> );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,11 @@ import rootReducer from "./reducers";
 import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ 
-//   trace: true, 
-//   traceLimit: 25 
-// }) || compose;
-const composeEnhancers = compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ 
+  trace: true, 
+  traceLimit: 25 
+}) || compose;
+// const composeEnhancers = compose;
 const middlewares = [sagaMiddleware, thunk];
 const store = createStore(
   rootReducer,
