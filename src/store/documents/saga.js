89513e8b22625367ec0b19a373e749ac3154d8fe
A1Exi1 <a.hany@exinitic.com>
2022-10-23 15:45:15 +0200

    remove console.log

--- src/store/documents/saga.js
+++ src/store/documents/saga.js
@@ -45,11 +45,9 @@ function * uploadDocs({ payload }){
   try {   
     const documents = [];
     for (const pair of payload.formData.entries()) {
-      console.log(pair);
       if (pair[1] !== "undefined")
         documents.push(pair[0]);
     }
-    console.log(documents);
     const data = yield call(documentsApi.uploadDocuments, payload);   
     yield put(uploadDocsSuccess(data));
     yield put(uploadDocsClear());
